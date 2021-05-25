b389a91e212b69e69fcafa229237c99dc8e0267a
Liudmylla <liudmyla12@yahoo.fr>
2021-05-25 14:58:38 +0200

    created property hasTreasure and method getRandomIsland

---
+++ src/Service/MapManager.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Service;
+
+use App\Entity\Tile;
+use App\Repository\TileRepository;
+
+class MapManager
+{
+    public function tileExists(int $x, int $y, TileRepository $tileRepository): bool
+    {
+         $tile = $tileRepository->findOneBy([],['coord_x' => $x,'coord_y'=>$y]);
+         if($tile){
+             return true;
+         }
+         return false;
+    }
+    public function getRandomIsland(TileRepository $tileRepository): Tile
+    {
+        $tiles = $tileRepository->findBy([],['type'=>'island']);
+        $index = array_rand($tiles);
+
+        return $tiles[$index];
+       
+    }
+}
